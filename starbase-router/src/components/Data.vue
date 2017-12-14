e6975dabf91fc5f94414e94ad879b56a79452827
mrszympek <piela.bartek@gmail.com>
2017-12-14 22:26:59 +0100

    starbase router app

---
+++ starbase-router/src/components/Data.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="">
+        Type: {{ type }}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                type: this.$route.params.type,
+                items: []
+            }
+        },
+        watch: {
+            '$route': 'fetchItems'
+        },
+        methods: {
+            fetchItems() {
+                this.items = []
+                let initial_ids = [1, 13, 14]
+
+                for (let i in initial_ids) {
+                    let id = initial_ids[i]
+                    console.log('id', id)
+                    fetch(`http://swapi.co/api/${this.type}/${id}`, {method: 'GET', type: 'cors'})
+                        .then(response => response.json())
+                        .then(json => this.items.push(json))
+                }
+            }
+        },
+        created() {
+            this.fetchItems();
+        }
+    }
+</script>
